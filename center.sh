1308f75ffd5818f60d9120268a7df3eea13f535d
lisbon <16432726+honeydatax@users.noreply.github.com>
2021-12-31 17:08:28 +0000

    ./center.sh

--- center.sh
+++ center.sh
@@ -1,23 +1,10 @@
-stars () {
-	for a in $(seq 0 $1)
-	do
-		printf "**"
-	done 
-}
-center () {
-	c=$(expr $1 '*' 2)
-	c=$(expr 80 - $c)
-	c=$(expr $c '/' 2)
-	for d in $(seq 0 $c)
-	do
-		printf " "
-	done 
+grap(){
+	a=$(./char $1 '**')
+	./center "$a"
 }
 
-printf "\ec\e[44;36m\a\nmacro graphics\n\n"
-for b in $(seq 0 10)
+printf "\ec\e[44;36m\n"
+for n in $(seq 1 10)
 do
-	center $b
-	stars $b
-	printf "\n"
-done
+	grap $n
+done 
