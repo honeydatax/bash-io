d9ea30de3229c6a68642abed782e7af77e138448
lisbon <16432726+honeydatax@users.noreply.github.com>
2021-12-31 19:40:38 +0000

    ./upper 'hello world.'

---
+++ upper.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+int main(int argc,char *argv[]){
+	int i;
+	int ii;
+	int iii;
+	char c[2080];
+	char *cc;
+	if (argc>1){
+		iii=strlen(argv[1]);
+		if (iii<2070){
+			strcpy(c,argv[1]);
+			for(i=0;i<iii;i++){
+				c[i]=toupper(c[i]);
+			}
+			printf("%s",c);
+		} 
+		
+	}else{	
+		printf("");
+	}
+	return 0;
+}
