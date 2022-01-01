1f68dc942db57efea15ab19c34772240c18e5e98
lisbon <16432726+honeydatax@users.noreply.github.com>
2022-01-01 17:09:00 +0000

    Temp file genaration

---
+++ rand.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+
+int main(int argc,char *argv[]){
+	int i;
+	int ii;
+	int iii;
+	srand(time(NULL));
+	printf("%d",rand());
+	return 0;
+}
